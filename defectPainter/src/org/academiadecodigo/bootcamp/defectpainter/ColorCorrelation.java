42956478aa4123019f90401c4d2739c827649d43
Joana Falcão <joanaferrofalcao@gmail.com>
2016-02-26 15:21:16 +0000

    v1.5 - added more colors

--- defectPainter/src/org/academiadecodigo/bootcamp/defectpainter/ColorCorrelation.java
+++ defectPainter/src/org/academiadecodigo/bootcamp/defectpainter/ColorCorrelation.java
@@ -8,12 +8,34 @@ import org.academiadecodigo.simplegraphics.graphics.Color;
  */
 public enum ColorCorrelation {
 
+ /*   public static final Color RED = new Color(255, 0, 0);
+    public static final Color GREEN = new Color(0, 255, 0);
+    public static final Color BLUE = new Color(0, 0, 255);
+    public static final Color WHITE = new Color(255, 255, 255);
+    public static final Color LIGHT_GRAY = new Color(192, 192, 192);
+    public static final Color GRAY = new Color(128, 128, 128);
+    public static final Color DARK_GRAY = new Color(64, 64, 64);
+    public static final Color BLACK = new Color(0, 0, 0);
+    public static final Color CYAN = new Color(0, 255, 255);
+    public static final Color MAGENTA = new Color(255, 0, 255);
+    public static final Color YELLOW = new Color(255, 255, 0);
+    public static final Color PINK = new Color(255, 175, 175);
+    public static final Color ORANGE = new Color(255, 200, 0);*/
+
+
+
     BLUE('b', Color.BLUE),
     BLACK('B', Color.BLACK),
     RED('r', Color.RED),
     GREEN('g', Color.GREEN),
     YELLOW('y', Color.YELLOW),
     ORANGE('o', Color.ORANGE),
+    LIGHT_GRAY('h', Color.LIGHT_GRAY),
+    GRAY('G', Color.GRAY),
+    DARK_GRAY('H', Color.DARK_GRAY),
+    CYAN('c', Color.CYAN),
+    MAGENTA('m', Color.MAGENTA),
+    PINK('p', Color.PINK),
     WHITE(' ', Color.WHITE);
 
     private char state;
@@ -54,6 +76,24 @@ public enum ColorCorrelation {
                 break;
             case 'o':
                 colorCorrelation = ColorCorrelation.ORANGE;
+                break;
+            case 'h':
+                colorCorrelation = ColorCorrelation.LIGHT_GRAY;
+                break;
+            case 'G':
+                colorCorrelation = ColorCorrelation.GRAY;
+                break;
+            case 'H':
+                colorCorrelation = ColorCorrelation.DARK_GRAY;
+                break;
+            case 'c':
+                colorCorrelation = ColorCorrelation.CYAN;
+                break;
+            case 'm':
+                colorCorrelation = ColorCorrelation.MAGENTA;
+                break;
+            case 'p':
+                colorCorrelation = ColorCorrelation.PINK;
                 break;
             case ' ':
                 colorCorrelation = ColorCorrelation.WHITE;
